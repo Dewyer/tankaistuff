f0e6ce717b6f1fcba2e52707d97d8bdd97c2edcc
Rátki Barnabás <barna.ratki@gmail.com>
2018-09-30 22:11:03 +0200

    Stufff

---
+++ TankFightingGameStuff/Assets/Menu/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+
+    IEnumerator Start()
+    {
+        WebSocket w = new WebSocket(new Uri("wss://localhost:44316/gamews"));
+        yield return StartCoroutine(w.Connect());
+        w.SendString("Hi there");
+        int i = 0;
+        while (true)
+        {
+            string reply = w.RecvString();
+            if (reply != null)
+            {
+                Debug.Log("Received: " + reply);
+                w.SendString("Hi there" + i++);
+            }
+            if (w.error != null)
+            {
+                Debug.LogError("Error: " + w.error);
+                break;
+            }
+            yield return 0;
+        }
+        w.Close();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ TankServers/TankServers/Services/GameServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TankServers.Services
+{
+    public interface IGameServer
+    {
+        void OnNewConnection(HttpContext context, WebSocket webSocket);
+    }
+
+    public class GameServer : IGameServer
+    {
+
+        public void OnNewConnection(HttpContext context, WebSocket webSocket)
+        {
+            Task.Run( async ()=>
+            {
+                await LoopWithConnection(context, webSocket);
+            });
+        }
+
+        private async Task LoopWithConnection(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = null;
+            try
+            {
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            }
+            catch (Exception e)
+            {
+                var a = e.Message;
+            }
+            string message1= Encoding.ASCII.GetString(buffer);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                string message = Encoding.ASCII.GetString(buffer);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
