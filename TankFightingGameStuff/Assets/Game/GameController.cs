1f00ec0adface315be412c89402a59e35ecb7442
Rátki Barnabás <barna.ratki@gmail.com>
2018-10-01 11:16:30 +0200

    szarshit

--- TankFightingGameStuff/Assets/Game/GameController.cs
+++ TankFightingGameStuff/Assets/Game/GameController.cs
@@ -12,11 +12,15 @@ public class GameController : MonoBehaviour
     public GameObject CodePanel;
     public bool CodeRunning = false;
     public bool Preparing = true;
-
+    public GameObject TankBlue;
+    public GameObject TankRed;
+    public GameObject Bullet;
+    public float tankSpeed;
+    public Queue<Action> CodeActions;
 
     // Use this for initialization
     void Start () {
-		
+        CodeActions = new Queue<Action>();
 	}
 	
 	// Update is called once per frame
@@ -24,13 +28,16 @@ public class GameController : MonoBehaviour
 		
 	}
 
-    public void RunCode(string code)
+    public void RunCode(string code,GameObject onObject)
     {
         var linesArr = code.Split('\n');
         var lines = linesArr.ToList().Select(x => x.ToLower().Trim()).ToList();
         var atCharCount = 0;
         var cleanCode = String.Join("",code.Trim().Split('\n'));
         var skipping = false;
+        CodeActions = new Queue<Action>();
+        CodeRunning = true;
+        UpdateDevVisible();
 
         for (int ii = 0; ii < lines.Count(); ii++)
         {
@@ -48,7 +55,7 @@ public class GameController : MonoBehaviour
                         for (int atLineInLoop = 0; atLineInLoop < lineLoop.Count; atLineInLoop++)
                         {
                             Debug.LogWarning("loop:"+lineLoop[atLineInLoop]);
-                            CallFunction(lineLoop[atLineInLoop]);
+                            CallFunction(lineLoop[atLineInLoop],onObject);
                         }
                     }
 
@@ -57,12 +64,14 @@ public class GameController : MonoBehaviour
                 else if ((!skipping) || lines[Math.Max(0,ii-1)].Contains("}"))
                 {
                     skipping = false;
-                    CallFunction(lines[ii]);
+                    CallFunction(lines[ii],onObject);
                 }
             }
 
             atCharCount += lines[ii].Length;
         }
+        if (CodeActions.Count != 0)
+            CodeActions.Dequeue()();
     }
 
     public string GetCodeBlock(string cleanCode,int atCharCount)
@@ -91,26 +100,133 @@ public class GameController : MonoBehaviour
         return inText;
     }
 
+    public void UpdateDevVisible()
+    {
+        if (Preparing && !CodeRunning)
+        {
+            CodePanel.SetActive(false);
+        }
+        else
+        {
+            CodePanel.SetActive(true);
+        }
+
+    }
+
     public void Test()
     {
         if (Preparing && !CodeRunning)
         {
             try
             {
-                RunCode(CodeInput.text);
+                RunCode(CodeInput.text,TankBlue);
             }
             catch (Exception e)
             {
+                CodeRunning = false;
+                UpdateDevVisible();
                 Debug.LogError(e.Message);
             }
         }
     }
 
-    public void CallFunction(string line)
+    public void TankShot(object tank)
+    {
+        Debug.Log("tank shot");
+    }
+
+    IEnumerator GoForward(int milis, GameObject onObject)
+    {
+        Debug.Log("nowfor");
+        onObject.GetComponent<Rigidbody2D>().velocity = onObject.transform.up * tankSpeed;
+        yield return new WaitForSeconds(milis / 1000f);
+        onObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        ActionEndCallBack();
+    }
+
+    IEnumerator Turn(bool isBal,int milis, GameObject onObject)
+    {
+        Debug.Log("rot");
+        var vel = 5 * (isBal?1:-1);
+        var timeelapsed = 0;
+        while (timeelapsed <= milis)
+        {
+            onObject.GetComponent<Rigidbody2D>().MoveRotation(onObject.transform.rotation.eulerAngles.z + vel);
+            timeelapsed += 10;
+            yield return new WaitForSeconds(10 / 1000f);
+        }
+        ActionEndCallBack();
+    }
+
+    IEnumerator Shoot(GameObject onObject)
+    {
+        var origin = onObject.GetComponentInChildren<Transform>().position;
+        var newBullet = Instantiate(Bullet, origin, onObject.transform.rotation);
+        newBullet.GetComponent<Rigidbody2D>().velocity = newBullet.transform.up * tankSpeed * 2;
+
+        yield return new WaitForSeconds(0.1f);
+        ActionEndCallBack();
+    }
+
+    public void ActionEndCallBack()
+    {
+        if (CodeRunning)
+        {
+            if (CodeActions.Count != 0)
+            {
+                CodeActions.Dequeue()();
+            }
+            else
+            {
+                CodeRunning = false;
+                UpdateDevVisible();
+            }
+        }
+    }
+
+    public int GetOneIntArg(string line)
+    {
+        try
+        {
+            var ii = int.Parse(line.Replace(')','(').Split('(')[1]);
+            return ii;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Cant parse function arg: " + ex.Message);
+        }
+        return 0;
+    }
+
+    public void CallFunction(string line,GameObject onObject)
     {
         if (line.StartsWith("test"))
         {
             Debug.Log("anyas");
+            return;
+        }
+        if (line.StartsWith("előre"))
+        {
+            var forMilis = GetOneIntArg(line);
+            CodeActions.Enqueue(() => { StartCoroutine(GoForward(forMilis, onObject)); });
+            return;
+        }
+        if (line.StartsWith("balra"))
+        {
+            var forMilis = GetOneIntArg(line);
+            CodeActions.Enqueue(() => { StartCoroutine(Turn(true,forMilis, onObject)); });
+            return;
+        }
+        if (line.StartsWith("jobbra"))
+        {
+            var forMilis = GetOneIntArg(line);
+            CodeActions.Enqueue(() => { StartCoroutine(Turn(false, forMilis, onObject)); });
+            return;
+        }
+        if (line.StartsWith("lő") && line.Contains("()"))
+        {
+            CodeActions.Enqueue(() => { StartCoroutine(Shoot(onObject)); });
             return;
         }
 
---
+++ TankFightingGameStuff/Assets/Game/TankShotDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShotDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        GameObject.FindGameObjectWithTag("GameController").SendMessage("TankShot", this);
+        Destroy(collision.gameObject);
+    }
+}
